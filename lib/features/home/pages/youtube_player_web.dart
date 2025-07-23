b82e71ba4d668a91089d895ede833658ac427b70
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-07-23 22:08:07 +0600

    `

--- lib/features/home/pages/youtube_player_web.dart
+++ lib/features/home/pages/youtube_player_web.dart
@@ -2,29 +2,31 @@
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
 
 // The following import is required for web platform view registry
-// ignore: undefined_prefixed_name
 void initializeYoutubePlayer(String viewType, String videoId) {
-  // Register the view factory
-  // ignore: undefined_prefixed_name
-  ui.platformViewRegistry.registerViewFactory(
-    viewType,
-    (int viewId) {
-      final iframe = html.IFrameElement()
-        ..width = '100%'
-        ..height = '100%'
-        ..src = 'https://www.youtube.com/embed/$videoId?autoplay=0&controls=1&rel=0'
-        ..style.border = 'none'
-        ..allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        ..allowFullscreen = true;
+  // Register the view factory only on web
+  if (kIsWeb) {
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      viewType,
+      (int viewId) {
+        final iframe = html.IFrameElement()
+          ..width = '100%'
+          ..height = '100%'
+          ..src = 'https://www.youtube.com/embed/$videoId?autoplay=0&controls=1&rel=0'
+          ..style.border = 'none'
+          ..allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          ..allowFullscreen = true;
 
-      // Add additional attributes for better compatibility
-      iframe.setAttribute('frameborder', '0');
-      iframe.setAttribute('allowfullscreen', '');
-      iframe.setAttribute('allow', 'autoplay; fullscreen');
+        // Add additional attributes for better compatibility
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('allow', 'autoplay; fullscreen');
 
-      return iframe;
-    },
-  );
+        return iframe;
+      },
+    );
+  }
 } 
